0bd5ddaf106d00c6259c16f79047650a5c687b55
Crocsx <Crocsx.gaming@gmail.com>
2016-04-12 02:36:34 +0200

    DAY 01

---
+++ Assets/Script/Player/PlayerInputs.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputs : MonoBehaviour {
+
+    // PROPERTIES -----------------------------------------------------
+
+    Transform           _transform;
+    PlayerController    _Controller;
+    Vector2             _currAxis;
+
+    // INTERFACE -----------------------------------------------------
+
+    void Start ()
+    {
+        _transform = transform;
+        _Controller = _transform.GetComponent<PlayerController>();
+    }
+	
+
+	void Update ()
+    {
+        CheckInputs();
+        _Controller.axisUpdate(new Vector3(_currAxis.y, 0, _currAxis.x));
+    }
+
+    // METHODS -----------------------------------------------------
+
+    void CheckInputs()
+    {
+        _currAxis.x = Input.GetAxis("Vertical");
+        _currAxis.y = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+            _Controller.onJump();
+    }
+}
---
+++ Assets/Script/Respawn/DeadZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZone : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform _entity = other.transform;
+
+        if (_entity.CompareTag("Player"))
+            _entity.GetComponent<PlayerController>().askDie();
+    }
+}
