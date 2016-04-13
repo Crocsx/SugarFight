7270945e6acba7a80d6aec676e1684c48fd98953
Clemoly <axelle.dru@gmail.com>
2016-04-13 16:21:36 +0200

    Je commit sinon on va tout m'effacer

---
+++ Assets/Script/Manager/MenuManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject bgClickToPlay;
+
+    bool isClicked;
+
+    private static MenuManager _instance;
+    private MenuManager() { }
+    public static MenuManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("MenuManager");
+                go.AddComponent<MenuManager>();
+                _instance = go.GetComponent<MenuManager>();
+            }
+            return _instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void salut()
+    {
+        Debug.Log("salut");
+    }
+
+    public void TitlecardClickedFirstTime()
+    {
+        Debug.Log("function");
+        if(!isClicked)
+        {
+            Debug.Log("clicked");
+            isClicked = true;
+            bgClickToPlay.transform.Translate(10, 0, 0);
+        }
+    }
+}
